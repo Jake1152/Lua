af20f1bfe99eb8db5aad6220fd51c168602f581c
Jinho <rancho974@gmail.com>
2024-12-11 09:55:23 +0900

    feat: lnc(lua netcat) unitcast version

--- subject/01_communicate/lnc.lua
+++ subject/01_communicate/lnc.lua
@@ -1,14 +1,7 @@
 local socket = require("socket")
 local host = "localhost"
 
--- 인자 간이 확인 버젼
-local validate_arugment = function()
-    if (not arg or not arg[1] or not arg[2]) then
-        return false
-    end
-    return true
-end
-
+-- # utiliy
 local sleep_ms = function(ms) 
     local time = os.clock()
     local ms = ms / 1000
@@ -22,24 +15,28 @@ local sleep = function(sec)
     end
 end
 
+-- # udp client-server code
+-- 인자 간이 확인 버젼
+local validate_arugment = function()
+    if (not arg or not arg[1] or not arg[2]) then
+        return false
+    end
+    return true
+end
 
 local server = function(port)
     local udp_server = socket.udp()
 
-    -- local ip = assert(udp_server.dns.toip(host))
-    -- local udp = assert(udp_server.udp())
     local ip = assert(socket.dns.toip(host))
     assert(udp_server:settimeout(0))  -- Non-blocking mode
     assert(udp_server:setsockname(ip, port))  -- 지정된 포트에서 서버 시작
     
-    -- print("UDP server start on " .. port)
     print("UDP server started on " .. host .. ":" .. port)
 
     while true do
         local data, ip, port = udp_server:receivefrom()
         if data then
             io.write(data)
-            -- assert(udp_server:sendto(data, ip, port))
         end
     end
 end
@@ -49,16 +46,15 @@ local client = function(port)
     local ip = assert(socket.dns.toip(host))
     
     --[[
-        assert를 하는 게 맞는가?
-        예외를 별도로 처리 하는 것이 맞는가?
+        - assert를 하는 게 맞는가?
+          예외를 별도로 처리 하는 것이 맞는가?
+        - 메시지 끝에 개행을 넣는 방식 vs 메시지 길이 정보를 패킷 형식으로 포함 하는 방식
     --]]
-    local data = "test\n"
+    local data = ""
     while (true) do
+        data = io.read() .. "\n"
         assert(udp_client:sendto(data, ip, port))
-        sleep(1)
     end
-    
-    
 end
 
 local main = function()
@@ -68,9 +64,7 @@ local main = function()
     end
     -- # 1. server, client 분기
     if (arg[1] == "-l") then
-        print("listen option!")
         local port = arg[3]
-        -- server
         server(port)
     else
         local port = arg[2]
